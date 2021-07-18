af82153a201ac04c02185242afc0786253ee3c96
Kirill Savchuk <kirillss1998@gmail.com>
2021-07-18 23:32:38 +0200

    Fixed failing test

--- src/test/java/lv/savchuk/weatherbyip/service/ip/GeolocationExternalClientManagerTest.java
+++ src/test/java/lv/savchuk/weatherbyip/service/ip/GeolocationExternalClientManagerTest.java
@@ -1,7 +1,5 @@
 package lv.savchuk.weatherbyip.service.ip;
 
-import feign.FeignException;
-import feign.Request;
 import javassist.NotFoundException;
 import lv.savchuk.weatherbyip.exception.ExternalClientException;
 import lv.savchuk.weatherbyip.model.dao.IpCoordinates;
@@ -14,11 +12,8 @@ import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
 
 import java.time.LocalTime;
-import java.util.Collections;
 import java.util.Set;
 
-import static feign.Request.Body;
-import static feign.Request.HttpMethod;
 import static java.lang.String.format;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.when;
@@ -41,19 +36,20 @@ public class GeolocationExternalClientManagerTest {
 	private IpCoordinates ipCoordinates;
 
 	@Before
-	public void setUp() {
+	public void setUp() throws ExternalClientException {
 		manager = new GeolocationExternalClientManager(Set.of(ipApiService, ipStackService));
 
 		ipCoordinates = IpCoordinates.builder().ipAddress(IP_ADDRESS).build();
 
 		when(ipApiService.getFailureRecoveryTime()).thenReturn(LocalTime.MAX);
 		when(ipStackService.getFailureRecoveryTime()).thenReturn(LocalTime.MAX);
+
+		when(ipApiService.getCoordinates(IP_ADDRESS)).thenReturn(ipCoordinates);
+		when(ipStackService.getCoordinates(IP_ADDRESS)).thenReturn(ipCoordinates);
 	}
 
 	@Test
-	public void getCoordinatesByIp_success_firstService() throws ExternalClientException, NotFoundException {
-		when(ipApiService.getCoordinates(IP_ADDRESS)).thenReturn(ipCoordinates);
-
+	public void getCoordinatesByIp_success_firstService() throws NotFoundException {
 		final IpCoordinates actualIpCoordinates = manager.getCoordinates(IP_ADDRESS);
 
 		assertThat(actualIpCoordinates).isEqualTo(ipCoordinates);
@@ -63,7 +59,6 @@ public class GeolocationExternalClientManagerTest {
 	public void getCoordinatesByIp_success_secondService() throws ExternalClientException, NotFoundException {
 		when(ipApiService.getCoordinates(IP_ADDRESS))
 			.thenThrow(new ExternalClientException("ERROR", "RESPONSE"));
-		when(ipStackService.getCoordinates(IP_ADDRESS)).thenReturn(ipCoordinates);
 
 		final IpCoordinates actualIpCoordinates = manager.getCoordinates(IP_ADDRESS);
 
@@ -81,11 +76,6 @@ public class GeolocationExternalClientManagerTest {
 			.thenThrow(new ExternalClientException("ERROR", "RESPONSE"));
 
 		manager.getCoordinates(IP_ADDRESS);
-	}
-
-	private FeignException newFeignException() {
-		final Request request = Request.create(HttpMethod.GET, "URL", Collections.emptyMap(), Body.create("{}"), null);
-		return new FeignException.BadRequest("BadRequest", request, "ERROR_RESPONSE".getBytes());
 	}
 
 }
